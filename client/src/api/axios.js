70eaf3714883e1b1a59afccad082c0de9a83f2f2
sushanthisodadasi@gmail.com <sushanthisodadasi@gmail.com>
2026-05-25 19:42:10 +0530

    Updated frontend API URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://team-task-manager-production-edef.up.railway.app/api"
 });
 
 API.interceptors.request.use((req) => {
